d896add0a009889e1c554760a3198dd8e29d0bdf
silas <silas@enzler.info>
2026-03-28 11:30:49 +0100

    added docker config and CD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react'
 import logo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [todos, setTodos] = useState([]);
   const [taskdescription, setTaskdescription] = useState("");
 
@@ -13,24 +11,24 @@ function App() {
   */
   const handleSubmit = event => {
     event.preventDefault();
-    console.log("Sending task description to Spring-Server: "+taskdescription);
-    fetch("http://localhost:8080/tasks", {  // API endpoint (the complete URL!) to save a taskdescription
+    console.log("Sending task description to Spring-Server: " + taskdescription);
+    fetch("/api/tasks", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ taskdescription: taskdescription }) // both 'taskdescription' are identical to Task-Class attribute in Spring
+      body: JSON.stringify({ taskdescription: taskdescription })
     })
-    .then(response => {
-      console.log("Receiving answer after sending to Spring-Server: ");
-      console.log(response);
-      window.location.href = "/";
-      setTaskdescription("");             // clear input field, preparing it for the next input
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        console.log("Receiving answer after sending to Spring-Server: ");
+        console.log(response);
+        window.location.href = "/";
+        setTaskdescription("");
+      })
+      .catch(error => console.log(error))
   }
 
-   /** Is called when ever the html input field value below changes to update the component's state.
+  /** Is called when ever the html input field value below changes to update the component's state.
   ** This is, because the submit should not take the field value directly.
   ** The task property in the state is used to store the current value of the input field as the user types into it.
   ** This is necessary because React operates on the principle of state and props, which means that a component's state
@@ -41,51 +39,49 @@ function App() {
     setTaskdescription(event.target.value);
   }
 
-
   /** Is called when the component is mounted (after any refresh or F5).
   ** It updates the component's state with the fetched todos from the API Endpoint '/'.
   */
   useEffect(() => {
-    fetch("http://localhost:8080/").then(response => response.json()).then(data => {
-      setTodos(data);
-    });
+    fetch("/api/")
+      .then(response => response.json())
+      .then(data => {
+        setTodos(data);
+      });
   }, []);
 
-
- /** Is called when the Done-Button is pressed. It sends a POST request to the API endpoint '/delete' and updates the component's state with the new todo.
+  /** Is called when the Done-Button is pressed. It sends a POST request to the API endpoint '/delete' and updates the component's state with the new todo.
   ** In this case if the task with the unique taskdescription is found on the server, it will be removed from the list.
   */
   const handleDelete = (event, taskdescription) => {
-    console.log("Sending task description to delete on Spring-Server: "+taskdescription);
-    fetch(`http://localhost:8080/delete`, { // API endpoint (the complete URL!) to delete an existing taskdescription in the list
+    console.log("Sending task description to delete on Spring-Server: " + taskdescription);
+    fetch("/api/delete", {
       method: "POST",
       body: JSON.stringify({ taskdescription: taskdescription }),
       headers: {
         "Content-Type": "application/json"
       }
     })
-    .then(response => {
-      console.log("Receiving answer after deleting on Spring-Server: ");
-      console.log(response);
-      window.location.href = "/";
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        console.log("Receiving answer after deleting on Spring-Server: ");
+        console.log(response);
+        window.location.href = "/";
+      })
+      .catch(error => console.log(error))
   }
-
-  // console.log("Current taskdescription: "+taskdescription);
 
   /**
    * render all task lines
    * @param {*} todos : Task list
    * @returns html code snippet
-  */
+   */
   const renderTasks = (todos) => {
     return (
       <ul className="todo-list">
         {todos.map((todo, index) => (
           <li key={todo.taskdescription}>
-            <span>{"Task " + (index+1) + ": "+ todo.taskdescription}</span>
-            <button onClick={(event) => handleDelete(event, todo.taskdescription) }>&#10004;</button>
+            <span>{"Task " + (index + 1) + ": " + todo.taskdescription}</span>
+            <button onClick={(event) => handleDelete(event, todo.taskdescription)}>&#10004;</button>
           </li>
         ))}
       </ul>
